a76f686604fcabdce14d62a26edb1016081969e0
Martin Poblete <martinpoblete9166@outlook.com>
2025-05-22 00:05:56 -0500

    Use UiAutomator2Options for capabilities in script instead of raw desired_caps json

--- test_appiumconnection.py
+++ test_appiumconnection.py
@@ -1,14 +1,18 @@
 import json
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 import time
 
 # Load desired capabilities from file
 with open("config/desired_caps.json", "r") as f:
     desired_caps = json.load(f)
 
+# Convert desired caps file to UiAutomator2Options
+options = UiAutomator2Options().load_capabilities(desired_caps)
+
 # Connect to Appium server
 print("Connecting to Appium...")
-driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+driver = webdriver.Remote("http://localhost:4723/wd/hub", options=options)
 
 print("Connected App launched.")
 
